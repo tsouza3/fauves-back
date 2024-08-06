ae78abb1023f0ce1351f7b3757f97b3d2fbb43df
Thiago de Souza <100713763+tsouza3@users.noreply.github.com>
2024-08-06 08:03:42 -0300

    Atualizar o auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,23 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-const protect = async (req, res, next) => {
+const protect = (requiredPermission) => async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        return res.status(401).json("Usuário não encontrado, não autorizado.");
+      }
+
+      if (requiredPermission && req.user.permissionCategory !== requiredPermission) {
+        return res.status(403).json("Acesso negado, permissões insuficientes.");
+      }
 
       next();
     } catch (error) {
