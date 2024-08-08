3cac1136b586d33dc286828b684bc2fcd7067d46
Thiago de Souza <100713763+tsouza3@users.noreply.github.com>
2024-08-08 20:44:32 -0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,8 +3,8 @@ import User from "../models/user.js";
 
 const getPermissionLevel = (permission) => {
   const levels = {
-    observer: 1,
-    user: 2,
+    user: 1,
+    observer: 2,
     seller: 3,
     admin: 4,
   };
@@ -22,24 +22,28 @@ const protect = (requiredPermission) => async (req, res, next) => {
       req.user = await User.findById(decoded.id).select("-password");
 
       if (!req.user) {
-        return res.status(401).json("Usuário não encontrado, não autorizado.");
+        return res.status(401).json({ message: "Usuário não encontrado, não autorizado." });
       }
 
       const userPermissionLevel = getPermissionLevel(req.user.permissionCategory);
       const requiredPermissionLevel = getPermissionLevel(requiredPermission);
 
       if (userPermissionLevel < requiredPermissionLevel) {
-        return res.status(403).json("Acesso negado, permissões insuficientes.");
+        return res.status(403).json({ message: "Acesso negado, permissões insuficientes." });
       }
 
       next();
     } catch (error) {
+      if (error.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: "Token expirado, por favor faça login novamente." });
+      }
+
       console.error("Erro na verificação do token:", error);
-      res.status(400).json("Token inválido, não autorizado.");
+      return res.status(400).json({ message: "Token inválido, não autorizado." });
     }
   } else {
     console.log("Token não fornecido ou em formato inválido.");
-    res.status(400).json("Token não fornecido ou em formato inválido.");
+    return res.status(400).json({ message: "Token não fornecido ou em formato inválido." });
   }
 };
 
