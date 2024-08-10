d78e460945a8dcb46cc503afdedcffc5445ffa24
Thiago de Souza <100713763+tsouza3@users.noreply.github.com>
2024-08-10 19:31:56 -0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,101 @@
-const eventoSchema = mongoose.Schema({
-  nomeEvento: { type: String, required: true },
-  dataInicio: { type: Date, required: true },
-  dataTermino: { type: Date, required: true },
-  categoria: { type: String, required: true },
-  localDoEvento: { type: String, required: true },
-  emailEvento: { type: String, required: true },
-  capaEvento: { type: String },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  producaoEvento: { type: mongoose.Schema.Types.ObjectId, ref: "CommercialProfile" },
-  members: [{
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    role: { type: String, enum: ['admin', 'observer', 'seller', 'checkin'], default: 'user' },
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    password: {
+      type: String,
+    },
+
+    QRCode: [{
+      type: String
   }],
-}, {
-  timestamps: true,
+   
+  txid: [{
+    type: String,
+}],
+    commercialProfiles: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "CommercialProfile" },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+  {
+    cpf: {
+      type: String,
+    },
+  },
+  {
+    
+  },
+ 
+  {
+    celular: {
+      type: String,
+    },
+  },
+  {
+    dataNascimento: {
+      type: String,
+    },
+  },
+  {
+    cep: {
+      type: String,
+    },
+  },
+  {
+    logradouro: {
+      type: String,
+    },
+  },
+
+  {
+    bairro: {
+      type: String,
+    },
+  },
+  {
+    cidade: {
+      type: String,
+    },
+  },
+  {
+    uf: {
+      type: String,
+    },
+  },
+  {
+    numero: {
+      type: Number,
+    },
+  },
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
-const Evento = mongoose.model("Evento", eventoSchema);
+const User = mongoose.model("User", userSchema);
 
-export default Evento;
+export default User;
